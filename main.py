7dc98f4790582a49c5d7be21ff3b8504aa960f44
Audrey Andoy <audreylandoy@gmail.com>
2020-09-05 00:07:24 +0000

    Added variables

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("Hello world")
+
+# VARIABLES - a way store a single piece of data
+# integer data type
+score = 0 
+health = 100
+# string data type
+name = "Zelda"
+
+print(health)
+print(score)
+print("Hello " + name)
+
+user = input("What is your name?")
+print("Hello " + user)
+
+# Ask the user for their favorite color. Then print out
+# their favorite color.
+color = input("What is your color?")
+print("Awesome! " + color + " is a great color!")
+ 
